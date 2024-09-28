715da01ae469d3cbc78cdfd68f54c6d410d1020e
axiomiety <axiomiety@gmail.com>
2024-09-28 16:46:08 +0800

    go-bt: start on the peer protocol

---
+++ src/data/peer_protocol.go
@@ -0,0 +1,35 @@
+package data
+
+import "bytes"
+
+type Handshake struct {
+	PstrLen  byte
+	Pstr     []byte
+	Reserved [8]byte
+	InfoHash [20]byte
+	PeerId   [20]byte
+}
+
+func (h *Handshake) ToBytes() []byte {
+	buffer := new(bytes.Buffer)
+	buffer.WriteByte(h.PstrLen)
+	buffer.Write(h.Pstr)
+	buffer.Write(h.Reserved[:])
+	buffer.Write(h.InfoHash[:])
+	buffer.Write(h.PeerId[:])
+	return buffer.Bytes()
+}
+
+func (m *Message) ToBytes() []byte {
+	buffer := new(bytes.Buffer)
+	buffer.Write(m.Length[:])
+	buffer.WriteByte(m.MessageId)
+	buffer.Write(m.Payload)
+	return buffer.Bytes()
+}
+
+type Message struct {
+	Length    [4]byte
+	MessageId byte
+	Payload   []byte
+}
